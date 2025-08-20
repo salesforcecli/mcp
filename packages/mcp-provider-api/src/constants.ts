1db874c6ab8ada36e0819e48553ebb13952d9b03
Stephen Carter <stephen.carter@salesforce.com>
2025-08-20 15:13:55 -0400

    chore: get unit tests working again in github

--- packages/mcp-provider-api/src/constants.ts
+++ packages/mcp-provider-api/src/constants.ts
@@ -1,1 +1,5 @@
-export const MCP_PROVIDER_API_VERSION: string = '1.0';
+import pkg from '../package.json' with { type: 'json' };
+
+const packageJson: {version: string} = pkg as {version: string};
+
+export const MCP_PROVIDER_API_VERSION: string = packageJson.version;
