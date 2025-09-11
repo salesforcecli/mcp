dde22fae247ddbc8175a20a377f1cc0abf392111
Sruti Paku <spaku@spaku-ltmc42q.internal.salesforce.com>
2025-09-11 00:00:03 -0700

    feat: add lwc experts mcp provider

--- packages/mcp/src/registry.ts
+++ packages/mcp/src/registry.ts
@@ -17,11 +17,12 @@
 import { McpProvider } from '@salesforce/mcp-provider-api';
 import { DxCoreMcpProvider } from '@salesforce/mcp-provider-dx-core';
 import { CodeAnalyzerMcpProvider } from '@salesforce/mcp-provider-code-analyzer';
-
+import { LwcExpertsMcpProvider } from '@salesforce/mcp-provider-lwc-experts';
 /** -------- ADD McpProvider INSTANCES HERE ------------------------------------------------------------------------- */
 
 export const MCP_PROVIDER_REGISTRY: McpProvider[] = [
   new DxCoreMcpProvider(),
   new CodeAnalyzerMcpProvider(),
+  new LwcExpertsMcpProvider(),
   // Add new instances here
 ];
