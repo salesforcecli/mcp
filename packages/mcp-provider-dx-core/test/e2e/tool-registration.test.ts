309c8a621bdc2f005fcd170292ef181b2a7469e0
Arun Tyagi <arun.tyagi@salesforce.com>
2026-02-05 16:49:36 +0530

    update test

--- packages/mcp-provider-dx-core/test/e2e/tool-registration.test.ts
+++ packages/mcp-provider-dx-core/test/e2e/tool-registration.test.ts
@@ -90,7 +90,7 @@ describe('specific tool registration', () => {
     try {
       const initialTools = (await client.listTools()).tools.map((t) => t.name).sort();
 
-      expect(initialTools.length).to.equal(7);
+      expect(initialTools.length).to.equal(8);
       expect(initialTools).to.deep.equal(
         [
           'run_soql_query',
@@ -100,6 +100,7 @@ describe('specific tool registration', () => {
           'run_code_analyzer',
           'list_code_analyzer_rules',
           'query_code_analyzer_results',
+          'get_ast_nodes_to_generate_xpath',
         ].sort(),
       );
     } catch (err) {
